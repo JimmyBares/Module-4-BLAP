c8185d9feabaaa40e2353b31cb435b2eb99e7cb5
JimmyBares <Jimmy.Bares04@gmail.com>
2025-08-04 18:49:59 -0400

    Module-4-BLAP

---
+++ index.js
@@ -0,0 +1,20 @@
+// template_u830ee2
+// service_zw9bhfr
+// DJddATGZ7UI7UpI2l
+
+function contact(event) {
+event.preventDefault();
+//emailjs
+//.sendForm(
+  //  'service_zw9bhfr',
+ //   'template_u830ee2',
+  //  event.target,
+ //   'DJddATGZ7UI7UpI2l' 
+//).then(() => {
+//console.log('this worked')
+//})
+setTimeout(() => {
+console.log('it worked 1')
+}, timeout);
+
+}
